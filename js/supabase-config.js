3ea98e33b8d8a41f7a76a8bb241ea6582bbd74a0
4wy2 <69701487+4wy2@users.noreply.github.com>
2026-06-18 13:27:49 +0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,8 +4,8 @@
 // =====================================================
 
 // حط بيانات مشروعك من Supabase هنا
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://gybksasoembmgdevogpv.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5YmtzYXNvZW1ibWdkZXZvZ3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3NzEyNjIsImV4cCI6MjA5NzM0NzI2Mn0.U0QFX7WS9ETbV0JmMYe-VNZhErJAPtZxwEBASep7AA4";
 
 // لا تعدل هذا السطر
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
